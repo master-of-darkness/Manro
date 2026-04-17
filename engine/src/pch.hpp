cfac1dd1f47c5baccac606528466d2a802437e9d
master-of-darkness <super.vava23.z@gmail.com>
2026-04-17 22:11:28 +0200

    feat: add precompiled header

---
+++ engine/src/pch.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+// Platform
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+#include <SDL3_mixer/SDL_mixer.h>
+
+// Vulkan
+#include <volk.h>
+#include <vk_mem_alloc.h>
+
+// Math
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Logging
+#include <spdlog/spdlog.h>
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+#include <functional>
+#include <algorithm>
+#include <array>
+#include <cstdint>
